240077b288394fc75f58d7fdc5defed4dceae7e2
OmondiKevin <kecovhi09@gmail.com>
2016-09-07 12:02:28 +0300

    new changes

---
+++ src/app/patient/patient.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-patient',
+  templateUrl: 'patient.component.html',
+  styleUrls: ['patient.component.css']
+})
+export class PatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
